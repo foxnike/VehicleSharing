c4cdffb3453bba29b8316610ad026325c296df6c
foxnike <foxnike@163.com>
2023-02-21 17:22:39 +0800

    implement add function

--- contract/sharing.sol
+++ contract/sharing.sol
@@ -2,10 +2,63 @@
 
 pragma solidity >=0.8.0;
 
-contract sharing{
+contract sharing {
     address payable admin;
+
+    struct vehicle {
+        string id;
+        uint256 unitPrice; // price per unit time;
+        uint256 idleTime;
+        uint256 drivingDistance; // accumulative driving distance
+        uint256 vehicleValue;
+    }
+
+    struct customer {
+        string id;
+        uint256 drivingAge;
+    }
+
+    vehicle[] private vehicleList;
+
+    customer[] private customerList;
+
+    modifier checkParameter() {
+        
+    }
 
     constructor() payable {
         admin = payable(msg.sender);
+    }
+
+    // need a modifier to check input parameter
+    function addVehicle(
+        string memory _id,
+        uint256 _unitPrice,
+        uint256 _idleTime,
+        uint256 _drivingDistance,
+        uint256 _vehicleValue
+    ) public  {
+        vehicleList.push(
+            vehicle({
+                id: _id,
+                unitPrice: _unitPrice,
+                idleTime: _idleTime,
+                drivingDistance: _drivingDistance,
+                vehicleValue: _vehicleValue
+            })
+        );
+    }
+
+    // need a modifier to check input parameter
+    function addCustomer(
+        string memory _id,
+        uint256 _drivingAge
+    ) public {
+        customerList.push(
+            customer({
+                id: _id,
+                drivingAge: _drivingAge
+            })
+        );
     }
 }
